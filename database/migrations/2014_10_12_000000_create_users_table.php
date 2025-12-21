6d4d0c6ee8cba4972d5e27675915e118cbde2926
Jeksen <hensiptong@gmail.com>
2025-12-21 13:49:52 +0700

    fix: increase kodeuser column size from 6 to 20 characters

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,7 +3,8 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CreateUsersTable extends Migration
+{
 
 	/**
 	 * Run the migrations.
@@ -12,22 +13,21 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users', function(Blueprint $table)
-		{
+		Schema::create('users', function (Blueprint $table) {
 			$table->increments('id');
 			$table->string('email')->unique();
 			$table->string('password', 60);
-			$table->string('kodeuser', 6);
+			$table->string('kodeuser', 20);
 			$table->text('alamat');
-			$table->string('nomorhandphone',15);
-			$table->string('saldo',30);
-			$table->string('nomorrekening',20);
-			$table->string('namarekening',50);
-			$table->string('fotoprofil',50);
-			$table->string('latitude',30);
-			$table->string('longtitude',30);
-			$table->string('statuspengguna',1);
-			$table->string('statusverifikasi',1);
+			$table->string('nomorhandphone', 15);
+			$table->string('saldo', 30);
+			$table->string('nomorrekening', 20);
+			$table->string('namarekening', 50);
+			$table->string('fotoprofil', 50);
+			$table->string('latitude', 30);
+			$table->string('longtitude', 30);
+			$table->string('statuspengguna', 1);
+			$table->string('statusverifikasi', 1);
 			$table->rememberToken();
 			$table->timestamps();
 		});
